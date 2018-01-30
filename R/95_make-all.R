621bc0ee2b0a5ba21204a5e4d7d11737c7cc2d18
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-01-30 14:41:29 -0800

    Re-render README in "make all" script

--- R/95_make-all.R
+++ R/95_make-all.R
@@ -12,3 +12,7 @@ if (!dir_exists(here("figs"))) {
 source(here("R/01_write-installed-packages.R"))
 source(here("R/02_wrangle-packages.R"))
 source(here("R/03_barchart-packages-built.R"))
+
+rmarkdown::render(here("README.Rmd"))
+## I'm not really sure why this is being created, tbh
+file_delete("README.html")
